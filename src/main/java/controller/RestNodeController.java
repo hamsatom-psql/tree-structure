c5ca6e510b62e401c13260a93bc2077e5f344e4c
Tomas Hamsa <okularek@centrum.cz>
2021-06-24 16:14:25 +0200

    Document controller in Swagger

--- src/main/java/controller/RestNodeController.java
+++ src/main/java/controller/RestNodeController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import model.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,7 +14,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 
-@RestController("/node")
+@RestController
 public class RestNodeController {
     private final INodeService service;
 
@@ -21,20 +23,35 @@ public class RestNodeController {
         this.service = service;
     }
 
-    @PostMapping(path = "/{id}")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Successfully created node"),
+            @ApiResponse(code = 400, message = "Invalid ids"),
+            @ApiResponse(code = 404, message = "No node with parentId"),
+            @ApiResponse(code = 409, message = "Duplicate nodeId"),
+    })
+    @PostMapping(path = "/node/{id}")
     @Nonnull
     public ResponseEntity<Object> createNode(@PathVariable @Nonnull String id, @RequestParam(required = false) @Nullable String parentId) {
         service.createNode(id, parentId);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping(path = "/{parentId}/descendants", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Successfully retrieved descendants of parentId"),
+            @ApiResponse(code = 404, message = "No node with parentId"),
+    })
+    @GetMapping(path = "/node/{parentId}/descendants", produces = MediaType.APPLICATION_JSON_VALUE)
     @Nonnull
     public List<Node> getDescendants(@PathVariable @Nonnull String parentId) {
         return service.getDescendants(parentId);
     }
 
-    @PutMapping(path = "/{id}")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Successfully changed node's parentId"),
+            @ApiResponse(code = 400, message = "Node can't be attached under provided parentId"),
+            @ApiResponse(code = 404, message = "No node for at least one of the provided ids"),
+    })
+    @PutMapping(path = "/node/{id}")
     @Nonnull
     public ResponseEntity<Object> changeParent(@PathVariable @Nonnull String id, @RequestParam(required = false) @Nullable String parentId) {
         service.updateNodeParent(id, parentId);
